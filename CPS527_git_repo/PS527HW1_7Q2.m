dd67a50fea852329463e58f83bd93ab989b97f1b
hujunwei <junweihu@Junweis-MacBook-Pro.local>
2014-09-03 08:26:44 -0400

    Set font size of Q7 p2
    Set font size of Q7 p2

---
+++ CPS527_git_repo/PS527HW1_7Q2.m
@@ -0,0 +1,14 @@
+%CPS527--HW1Q7_2
+clc;
+clear all;
+S = load ('hw1.mat');
+Pxgc = S.pxgc;
+
+Pxgb = S.pxgb;
+stairs(Pxgc);
+hold on
+stairs(Pxgb, 'color', 'red');
+title('Conditional probabilities p(x|c) & p(x|b)','fontsize',18);
+xlabel('Pixel values','fontsize',18);
+ylabel('Probability','fontsize',18);
+legend('Pxgc','Pxgb');
